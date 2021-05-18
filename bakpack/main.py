7c627713d6054aa4daef74fe214e680a3d4c7ab4
Tom Soulanille <soul@prama.com>
2021-05-17 19:35:47 -0700

    tweaks; blackening

--- bakpack/main.py
+++ bakpack/main.py
@@ -4,42 +4,63 @@ from absl import flags
 from absl import logging
 
 from collections import defaultdict
+import json
 from pathlib import Path
+from sys import stdout
 
 from .packer import binpack
 
 FLAGS = flags.FLAGS
 
 flags.DEFINE_integer("size", None, "How much a bin can hold", lower_bound=1)
-
-# (Path('d1') / Path('1.txt')).stat().st_size
+flags.DEFINE_string("output", None, "Output filename (defaults to stdout)")
+flags.DEFINE_bool("json", False, "Output only JSON of files with sizes")
 
 
 def main(argv):
     logging.info(f"Bin size is {FLAGS.size}")
     logging.info(f"args are {argv}")
-    paths_of_size = defaultdict(list)
+    pathnames_of_size = defaultdict(list)
     sizes = []
     for pathname in argv[1:]:
         path = Path(pathname)
         size = path.stat().st_size
         sizes.append(size)
-        paths_of_size[size].append(path)
-    logging.info(f"paths_of_size is {paths_of_size}")
+        pathnames_of_size[size].append(pathname)
+    logging.info(f"pathnames_of_size is {pathnames_of_size}")
     logging.info(f"sizes: {sizes}")
     packed = binpack(FLAGS.size, sizes)
     logging.info(f"packed {packed}")
-    print("# Some overall preamble")
+
+    pathed = []
     for bin_contents in packed:
-        print(f"# Some preamble to a particular tape")
+        paths = []
         for size in bin_contents:
-            path = paths_of_size[size].pop()
-            print(f"# process Path {path}")
-        print(f"# Some post-process for a particular tape")
-    print(f"# Any overall post-process")
+            paths.append((pathnames_of_size[size].pop(), size))
+        pathed.append(paths)
+
+    with get_outfile() as f:
+        if FLAGS.json:
+            json.dump(pathed, f)
+            return
+        f.write("# Some overall preamble\n")
+        for bin_contents in pathed:
+            f.write(f"# Some preamble to a particular tape\n")
+            for pathname, size in bin_contents:
+                f.write(f"# process Path {pathname}\n")
+            f.write(f"# Some post-process for a particular tape\n")
+        f.write(f"# Any overall post-process\n")
+
+
+def get_outfile():
+    if FLAGS.output is None:
+        return stdout
+    return open(FLAGS.output, "w")
+
 
 def entry():
     app.run(main)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(main)
--- bakpack/packer.py
+++ bakpack/packer.py
@@ -1,6 +1,7 @@
 from ortools.linear_solver import pywraplp
 from typing import Union
 
+
 def binpack(capacity: int, weights: list) -> Union[None, list[list[int]]]:
     """Pack widgets into minimum number of bins
 
@@ -13,39 +14,54 @@ def binpack(capacity: int, weights: list) -> Union[None, list[list[int]]]:
     n_bins = n_widgets
 
     # from https://developers.google.com/optimization/bin/bin_packing#python_7
-    
+
     # Create the mip solver with the SCIP backend.
-    solver = pywraplp.Solver.CreateSolver('SCIP')
+    solver = pywraplp.Solver.CreateSolver("SCIP")
 
     # Variables
     # x[i, j] = 1 if item i is packed in bin j.
-    x = dict(((i, j), solver.IntVar(0, 1, f'x_{i}_{j}')) for i in range(n_widgets) for j in range(n_widgets))
+    x = dict(
+        ((i, j), solver.IntVar(0, 1, f"x_{i}_{j}"))
+        for i in range(n_widgets)
+        for j in range(n_widgets)
+    )
 
     # y[j] = 1 if bin j is used.
-    y = dict((j, solver.IntVar(0, 1, f'y[{j}]')) for j in range(n_bins))
+    y = dict((j, solver.IntVar(0, 1, f"y[{j}]")) for j in range(n_bins))
 
     # Constraints
     # Each item must be in exactly one bin.
     for i in range(n_widgets):
         solver.Add(sum(x[i, j] for j in range(n_bins)) == 1)
-        
+
     # The amount packed in each bin cannot exceed its capacity.
     for j in range(n_bins):
-        solver.Add(sum(x[i, j] * weights[i] for i in range(n_widgets)) <= y[j] * capacity)
-            
+        solver.Add(
+            sum(x[i, j] * weights[i] for i in range(n_widgets)) <= y[j] * capacity
+        )
+
     # Objective: minimize the number of bins used.
     solver.Minimize(solver.Sum([y[j] for j in range(n_bins)]))
 
     status = solver.Solve()
     if status == pywraplp.Solver.OPTIMAL:
-        #print('Time = ', solver.WallTime(), ' milliseconds')
+        # print('Time = ', solver.WallTime(), ' milliseconds')
         bin_item_weights = []
         for j in range(n_bins):
             if y[j].solution_value() == 1:
-                bin_item_weights.append(sorted((weights[i] for i in range(n_widgets) if x[i, j].solution_value() > 0), reverse=True))
+                bin_item_weights.append(
+                    sorted(
+                        (
+                            weights[i]
+                            for i in range(n_widgets)
+                            if x[i, j].solution_value() > 0
+                        ),
+                        reverse=True,
+                    )
+                )
         bin_item_weights.sort(reverse=True)
-        #print(f"bin contents {bin_item_weights}")
+        # print(f"bin contents {bin_item_weights}")
         return bin_item_weights
     else:
-        #print('The problem does not have an optimal solution.')
+        # print('The problem does not have an optimal solution.')
         return None
